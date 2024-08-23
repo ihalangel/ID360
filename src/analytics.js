95af661fe0af856f218ba1170affbfb54080e19b
ihalangel <ihalangel@gmail.com>
2024-08-23 05:17:39 +0000

    Analytic on

--- src/analytics.js
+++ src/analytics.js
@@ -1,7 +1,7 @@
 
 import ReactGA from 'react-ga4';
 
-const GA_MEASUREMENT_ID = 'G-6N4TPWR8JE'; // Reemplaza con tu ID de seguimiento
+const GA_MEASUREMENT_ID = 'G-9R4Q3DF83'; // Reemplaza con tu ID de seguimiento
 
 export const initializeAnalytics = () => {
   ReactGA.initialize(GA_MEASUREMENT_ID);
